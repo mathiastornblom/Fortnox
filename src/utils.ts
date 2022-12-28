bdb424c7011b02a1e927d6ba701ca71e6795ba29
Mathias Törnblom <mathias@tornbloms.net>
2022-12-28 21:41:25 +0100

    Fixed GetAllItems with filter

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,54 @@
-import { Dispatch } from "./dispatch";
+import { Dispatch } from './dispatch';
 
 export class Util {
-    static async getAllPages(path: string, arrayName: string, dispatch: Dispatch, filter?: string) {
+    static async getAllPages(
+        path: string,
+        arrayName: string,
+        dispatch: Dispatch,
+        filter?: string
+    ) {
+        if (filter) path += '?filter=' + filter;
+
         const items: any = await dispatch.get(path);
-        
-        if (filter)
-            path += '?filter=' + filter;
 
         let allItems: any[] = [];
-        const totalPages = Number.parseInt(items.MetaInformation['@TotalPages']);
+        const totalPages = Number.parseInt(
+            items.MetaInformation['@TotalPages']
+        );
         allItems.push(...items[arrayName]);
-        
-        if (totalPages > 1){
+
+        if (totalPages > 1) {
             let currentPage: number = 2;
-            while(currentPage <= totalPages) {
-                const pageItems: any = await dispatch.get(path + (filter ? '&' : '?') + 'page=' + currentPage);
+            while (currentPage <= totalPages) {
+                const pageItems: any = await dispatch.get(
+                    path + (filter ? '&' : '?') + 'page=' + currentPage
+                );
                 allItems.push(...pageItems[arrayName]);
                 currentPage++;
             }
         }
-        
+
         return allItems;
     }
 
     // convert fortnox error into standard error
-    static async makeErrorFromResponse (response: any) {
-
+    static async makeErrorFromResponse(response: any) {
         // get fortnox error if exist
-        if (response?.headers?.get('content-type')?.startsWith('application/json')) {
+        if (
+            response?.headers
+                ?.get('content-type')
+                ?.startsWith('application/json')
+        ) {
             const fnError = await response.json();
             const fnMessage = fnError?.ErrorInformation?.message;
             if (fnMessage) {
                 return new Error(fnMessage);
             }
         }
-    
-        return new Error(response.statusText ?? "An error has occured");
+
+        return new Error(response.statusText ?? 'An error has occured');
     }
-    
+
     // a promise that resolves after specified amount of time
     static delay(ms: number): Promise<void> {
         return new Promise((resolve: () => void) => {
--- test/http-server-oauth-authorize.ts
+++ test/http-server-oauth-authorize.ts
@@ -1,6 +1,6 @@
 import http from 'http';
 import fetch from 'node-fetch';
-import config from './fnConfig_sample.json';
+import config from './fnConfig.json';
 
 /**
  * Simple server to get authorization code from Fortnox using OAuth2 (now it is the only supported method for the new apps)
@@ -18,63 +18,68 @@ import config from './fnConfig_sample.json';
 
 const host = 'localhost';
 const port = 3333;
-const scope = 'article customer costcenter price invoice supplier supplierinvoice';
+const scope =
+    'article customer costcenter price invoice supplier supplierinvoice';
 
 const redirect_uri = `http://${host}:${port}/authorize`;
 
 const fortnoxAuthReq = {
-  client_id: config.clientId,
-  redirect_uri,
-  scope,
-  state: `XXX`,
-  access_type: `offline`,
-  response_type: `code`
+    client_id: config.clientId,
+    redirect_uri,
+    scope,
+    state: `XXX`,
+    access_type: `offline`,
+    response_type: `code`,
 };
 
-const authUrl = `https://apps.fortnox.se/oauth-v1/auth?${makeQueryString(fortnoxAuthReq)}`;
+const authUrl = `https://apps.fortnox.se/oauth-v1/auth?${makeQueryString(
+    fortnoxAuthReq
+)}`;
 
-function makeQueryString (params: any) {
-  return Object.keys(params)
-    .map((key: string) => `${key}=${encodeURIComponent(params[key])}`)
-    .join('&');
+function makeQueryString(params: any) {
+    return Object.keys(params)
+        .map((key: string) => `${key}=${encodeURIComponent(params[key])}`)
+        .join('&');
 }
 
 async function authorize(url: string, params: any, headers?: any) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            ...headers,
+        },
+        body: makeQueryString(params),
+    });
 
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      ...headers
-    },
-    body: makeQueryString(params)
-  });
-
-  return await response.json();
+    return await response.json();
 }
 
 const server = http.createServer((req, res) => {
+    const url = new URL(`http://${host}:${port}${req.url}`);
+    if (url.pathname === '/authorize') {
+        const params = {
+            code: url.searchParams.get('code'),
+            grant_type: `authorization_code`,
+            redirect_uri,
+        };
 
-  const url = new URL(`http://${host}:${port}${req.url}`);
-  if (url.pathname === '/authorize') {
-    const params = {
-      code: url.searchParams.get('code'),
-      grant_type: `authorization_code`,
-      redirect_uri,
+        const credentials = Buffer.from(
+            `${config.clientId}:${config.clientSecret}`
+        ).toString('base64');
+
+        authorize('https://apps.fortnox.se/oauth-v1/token', params, {
+            Authorization: `Basic ${credentials}`,
+        }).then((json) => {
+            res.writeHead(200);
+            res.end(json.access_token);
+        });
+    } else {
+        res.writeHead(200);
+        res.end(`<a href="${authUrl}">authorize</a>`);
     }
-    
-    const credentials = Buffer.from(`${config.clientId}:${config.clientSecret}`).toString('base64');
-  
-    authorize('https://apps.fortnox.se/oauth-v1/token', params, { 'Authorization': `Basic ${credentials}` }).then(json => {
-      res.writeHead(200);
-      res.end(json.access_token);
-    });
-  } else {
-    res.writeHead(200);
-    res.end(`<a href="${authUrl}">authorize</a>`)
-  }
 });
 
 server.listen(port, host, () => {
-  console.log(`server started on http://${host}:${port}`);
+    console.log(`server started on http://${host}:${port}`);
 });
